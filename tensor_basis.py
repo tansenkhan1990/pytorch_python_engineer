8629e642b03a3ce4ffd4cc445b1e716b9123ffef
tansen <tansenkhan1990@gmail.com>
2021-03-30 01:07:28 +0200

    add new tensor and list to tensor

--- tensor_basis.py
+++ tensor_basis.py
@@ -6,3 +6,7 @@ y = torch.zeros(3, 2 ,5)
 print('x datatype : ', x.dtype)
 print('x datatype : ', y.dtype)
 print('x size : ', x.size())
+# list to tensor
+newtensor = torch.tensor([5,3,1,2,3,4,5,66])
+print('this is a newtensor')
+print(newtensor)
