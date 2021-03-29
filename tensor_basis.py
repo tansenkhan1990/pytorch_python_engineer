2fb301e0416f0804f2579228ef1c9defbd42732c
tansen <tansenkhan1990@gmail.com>
2021-03-30 01:15:29 +0200

    row and column operations

--- tensor_basis.py
+++ tensor_basis.py
@@ -3,10 +3,18 @@ import torch
 x = torch.ones(3, 2 ,5)
 y = torch.zeros(3, 2 ,5)
 
-print('x datatype : ', x.dtype)
-print('x datatype : ', y.dtype)
-print('x size : ', x.size())
-# list to tensor
-newtensor = torch.tensor([5,3,1,2,3,4,5,66])
-print('this is a newtensor')
-print(newtensor)
+# print('x datatype : ', x.dtype)
+# print('x datatype : ', y.dtype)
+# print('x size : ', x.size())
+# # list to tensor we can also tranfer numpy array or list into a tensor
+# newtensor = torch.tensor([5,3,1,2,3,4,5,66])
+# print('this is a newtensor')
+# print(newtensor)
+# row and column operation
+rowAndColumn = torch.rand(3,4)
+print('row and column')
+print(rowAndColumn)
+print('first row and all column')
+print(rowAndColumn[1,:])
+print('all row and first column')
+print(rowAndColumn[:,1])
