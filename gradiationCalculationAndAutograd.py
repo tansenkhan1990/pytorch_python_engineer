af9d0e145a68f2cf6b526c17689afe88dda50310
tansen <tansenkhan1990@gmail.com>
2021-03-30 02:57:14 +0200

    calculate gradient with vector

--- gradiationCalculationAndAutograd.py
+++ gradiationCalculationAndAutograd.py
@@ -2,6 +2,8 @@ import torch
 x = torch.rand(3 , requires_grad= True)
 print(x)
 y = x * 4
-y = y.mean()
-y.backward()
+# y = y.mean()
+v = torch.tensor([1,3,4] , dtype = torch.float64)
+y.backward(v)
+print('this is gradient value')
 print(x.grad)
