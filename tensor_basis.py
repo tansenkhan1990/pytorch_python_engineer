bfd875566136abd52b3a43f7a0c61d7d949afe01
tansen <tansenkhan1990@gmail.com>
2021-03-30 02:02:16 +0200

    view method to convert

--- tensor_basis.py
+++ tensor_basis.py
@@ -1,24 +1,4 @@
 import torch
-
-x = torch.ones(3, 2 ,5)
-y = torch.zeros(3, 2 ,5)
-
-# print('x datatype : ', x.dtype)
-# print('x datatype : ', y.dtype)
-# print('x size : ', x.size())
-# # list to tensor we can also tranfer numpy array or list into a tensor
-# newtensor = torch.tensor([5,3,1,2,3,4,5,66])
-# print('this is a newtensor')
-# print(newtensor)
-# row and column operation
-rowAndColumn = torch.rand(3,4)
-print('row and column')
-print(rowAndColumn)
-print('first row and all column')
-print(rowAndColumn[1,:])
-print('all row and first column')
-print(rowAndColumn[:,1])
-print('first element of first row')
-print(rowAndColumn[1,1])
-print('first element of first row actual value')
-print(rowAndColumn[1,1].item())
+a = torch.range(1, 16)
+b = a.view(4, 4)
+print(b)
